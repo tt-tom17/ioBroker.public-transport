be277a3570c09fc50f43d276832a8121463789f6
tt-tom17 <tgb@kabelmail.de>
2025-12-06 19:40:36 +0100

    feat: add VendoService type to TTAdapter class

--- src/lib/types/TTAdapter.d.ts
+++ src/lib/types/TTAdapter.d.ts
@@ -1,4 +1,5 @@
 import * as utils from '@iobroker/adapter-core';
+import type { VendoService } from '../class/dbVendoService';
 import type { DepartureRequest } from '../class/departure';
 import type { HafasService } from '../hafasService';
 import type { Library } from '../tools/library';
@@ -6,6 +7,7 @@ import type { Library } from '../tools/library';
 declare class TTAdapter extends utils.Adapter {
     library: Library;
     hService: HafasService;
+    vService: VendoService;
     depRequest: DepartureRequest;
     unload: boolean;
 }
